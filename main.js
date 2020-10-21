d781611746791040b1f6c57d742a58afd9cf5e93
Adeyemi Misturah <adunola142@gmail.com>
2020-10-21 13:45:22 -0500

    A Javascript Function that outputs my details using Object Dot Notation

---
+++ main.js
@@ -0,0 +1,12 @@
+function myDetails(){
+    var person = new Object();
+person.firstName = "Adeyemi";
+person.lastName = "Misturah";
+person.country = "Nigeria";
+person.proffesion = "Web Developer"; 
+
+console.log("My Name is" + " " + person.firstName + " " + person.lastName +" " + "from" + " " + person.country + ", " + "i am a" +" " +  person.proffesion)
+
+} 
+
+console.log(myDetails());
